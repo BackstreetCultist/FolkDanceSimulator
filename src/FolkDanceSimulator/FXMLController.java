e52fd9d380e7c6650ac96832c0814a71f3758cc0
BackstreetCultist <charlie@101a.co.uk>
2020-01-14 15:09:43 +0000

    Add resource folder, add FXML support and FXML menu skeleton

---
+++ src/FolkDanceSimulator/FXMLController.java
@@ -0,0 +1,4 @@
+package FolkDanceSimulator;
+
+public class FXMLController {
+}
--- src/FolkDanceSimulator/Main.java
+++ src/FolkDanceSimulator/Main.java
@@ -1,27 +1,23 @@
 package FolkDanceSimulator;
 
 import javafx.application.Application;
-import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Scene menuScene;
-    Pane menuPane;
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Folk Dance Simulator 2020");
+        AnchorPane root = FXMLLoader.load(getClass().getResource("FXMLSource/Menu.fxml"));
 
-        //Generate menu
-        menuPane = new Pane();
-        menuScene = new Scene(menuPane, 800, 600);
+        Scene menuScene = new Scene(root, 600, 400);
+
+        primaryStage.setTitle("FXML Welcome");
         primaryStage.setScene(menuScene);
         primaryStage.show();
-    }
-
+}
 
     public static void main(String[] args) {
         launch(args);
